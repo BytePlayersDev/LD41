7a019c18e0c77f1b4bad93141b849ddb2f5a163a
vtobes <vtobes@gmail.com>
2018-04-21 16:07:07 +0200

    Creado componente para asociar la carta a un gameobject

---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDisplay : MonoBehaviour {
+
+    public Card Card;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
