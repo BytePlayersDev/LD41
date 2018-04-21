df6f771a832267529229cf7b94f0700cbf41dee9
nama1510 <nama1510@gmail.com>
2018-04-21 20:16:01 +0200

    Basic Enemy

---
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour {
+
+    protected enum State {
+        Patrol,
+        Chasing,
+        Shooting
+    }
+
+    protected State currentState = State.Patrol;
+
+    #region Variables
+    //Rigidbody2D rb;
+
+    //Nodes for patrol behavior
+    //This is set for only 2 waypoints
+    public Transform[] waypoints;
+    protected int waypointID;
+    protected bool samePosition;
+
+    [HideInInspector]
+    public Transform activeNode;
+
+    public float moveSpeed;
+    protected float decreaseSpeedFactor = 50;
+    protected Vector2 direction;
+    //Related to Player variables
+    public GameObject player;
+
+    //Graphic varibles
+    private bool facingRight;
+    //TODO: Add animator
+    //TODO: Add enemy sprite
+
+    //Attribute variables
+    protected bool isAlive;
+    protected bool isAttacking;
+    #endregion
+
+    #region Custom Functions
+    /// <summary>
+    /// Flip Enemy Sprite
+    /// </summary>
+    protected void FlipSprite() {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        direction *= -1;
+        
+    }
+    /// <summary>
+    /// Changes Waypoint
+    /// </summary>
+    protected void ChangeWaypoint() {
+        if (waypointID == 0){
+
+            waypointID = 1;
+            facingRight = false;
+            FlipSprite();
+        }
+        else {
+            waypointID = 0;
+            facingRight = true;
+            FlipSprite();
+        }
+    }
+
+    /// <summary>
+    /// Displays enemy death
+    /// </summary>
+    protected void Die() {
+
+        //TODO: Destroy GameObject and children
+        //TODO: Play Death animation
+        isAlive = false;
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            samePosition = true;
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            samePosition = false;
+        }
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/Enemies/RockGuy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockGuy : EnemyBase {
+    #region Variables
+    #endregion
+
+    #region Unity Methods
+    // Use this for initialization
+    void Start () {
+        if (moveSpeed == 0) moveSpeed = 2;
+        direction = new Vector2(transform.localScale.x, 0);
+        samePosition = false;
+        isAlive = true;
+        currentState = EnemyBase.State.Patrol;
+
+        if (waypoints == null) Debug.LogError("Assign wayponits to " + this.gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        switch (currentState) {
+            case EnemyBase.State.Patrol:
+                Patrol();
+                break;
+        }
+	}
+
+    #endregion
+
+    #region Custom Functions
+    /// <summary>
+    /// Simple Patrol Function
+    /// </summary>
+    private void Patrol() {
+        if (Mathf.Abs(Vector2.Distance(waypoints[waypointID].position, transform.position)) < .5f && !samePosition) {
+            ChangeWaypoint();
+        }
+
+        if (!samePosition) {
+            Vector2 translation = Vector2.MoveTowards(this.transform.position, waypoints[waypointID].transform.position, moveSpeed / decreaseSpeedFactor);
+            transform.position = new Vector2(translation.x, transform.position.y);
+        }
+    }
+    #endregion
+}
