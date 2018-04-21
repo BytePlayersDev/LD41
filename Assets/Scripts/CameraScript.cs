316bf30523c0a842b354de47cc2cbba84255471c
nama1510 <nama1510@gmail.com>
2018-04-21 23:31:11 +0200

    Camera Movement

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public float speed;
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += Vector3.up * speed / 100; 
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("Player enetered trigger.");
+            //TODO: Respawn player
+        }
+    }
+}
