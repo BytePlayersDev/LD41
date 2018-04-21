0f10d62f4c565165bb447062670a8b8eeea363d7
vtobes <vtobes@gmail.com>
2018-04-21 16:03:19 +0200

    Añadido forma de crear cartas como assets

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class Card :ScriptableObject {
 
     //Sprite de la carta
