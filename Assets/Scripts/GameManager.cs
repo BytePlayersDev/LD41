838d1c7b0ae6ac729ee22c5d0934d18663da096b
Varo Hurtado <ahs007@gmail.com>
2018-04-21 18:27:48 +0200

    GameManager Prototype

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public PlayerController playerController;
+    public GameObject scoreController;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void DoAction(CardActionEnum.Action action)
+    {
+
+    }
+}
