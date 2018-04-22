7edc2bd8d5827c35eda4d4ef4d8a2ec40eb83e67
Varo Hurtado <ahs007@gmail.com>
2018-04-22 17:41:32 +0200

    Uploaded 'Splashscreen'

---
+++ Assets/_Scripts/SplashFade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashFade : MonoBehaviour {
+
+    public Image logo;
+    
+    IEnumerator Start()
+    {
+        logo.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(3.5f);
+
+        SceneManager.LoadScene("Menu");
+
+    }
+
+    void FadeIn()
+    {
+        logo.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        logo.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
