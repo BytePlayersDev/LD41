5dab01731b9e97a7f42ca89eef9a18e251c39d36
Varo Hurtado <ahs007@gmail.com>
2018-04-22 19:38:18 +0200

    Pause Menu, Verified. Game Over Menu, prototyped.

--- Assets/_Scripts/Managers/UIManager.cs
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
     #region UI Variables
-    [SerializeField] private GameObject pauseWindow;
+    [SerializeField] private GameObject window;
     [SerializeField] private CameraScript cs;
+    [SerializeField] private LevelManager lm;
+
+    private Text windowTxt;
     #endregion
     #region Unity Methods
     private void Start()
     {
-        if (pauseWindow == false) {
+        if (window == false) {
             Debug.LogError("Pause menu is null in " + this.gameObject.name);
         }
     }
@@ -20,29 +24,41 @@ public class UIManager : MonoBehaviour {
 
     #region UI Functions
     public void PauseButtonPressed() {
-        if (!pauseWindow.activeInHierarchy){
-            pauseWindow.SetActive(true);
+
+        windowTxt = window.transform.GetChild(1).GetComponent<Text>();
+        windowTxt.text = "PAUSE";
+
+        if (!window.activeInHierarchy){
+            window.SetActive(true);
             Time.timeScale = 0;
             cs.SetGamePaused(true);
         }
         else{
-            pauseWindow.SetActive(false);
+            window.SetActive(false);
             cs.SetGamePaused(false);
             Time.timeScale = 1;
         }
     }
 
-    public void RestartButtonPressed() {
-        //if (Time.timeScale != 1)
-        //    Time.timeScale = 1;
-        //TODO: restart game Scene.
+    public void ShowGameOver()
+    {
+        windowTxt = window.transform.GetChild(1).GetComponent<Text>();
+        windowTxt.text = "GameOver";
 
+        if (!window.activeInHierarchy)
+        {
+            window.SetActive(true);
+            Time.timeScale = 0;
+            cs.SetGamePaused(true);
+        }
+    }
+
+    public void RestartButtonPressed() {
+        lm.LoadLevel();
     }
 
     public void MenuButtonPressed() {
-        //if (Time.timeScale != 1)
-        //    Time.timeScale = 1;
-        //TODO: load Menu Scene.
+        lm.LoadMainMenu();
     }
     
     #endregion
