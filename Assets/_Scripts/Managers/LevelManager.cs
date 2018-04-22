3e048af7472d5334000de67450a681fd8573242a
Varo Hurtado <ahs007@gmail.com>
2018-04-22 16:51:40 +0200

    Main menu functional

---
+++ Assets/_Scripts/Managers/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    #region Custom Functions
+
+    // Loads the Level Scene
+    public void LoadLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main");
+    }
+    
+    // Loads the Main Menu Scene
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Loads the Credits Scene
+    public void LoadCredits()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Credits");
+    }
+
+    // Closes the game
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+}
