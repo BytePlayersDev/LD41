23286b37c30966b692552465b94d38f3cb858e66
nama1510 <nama1510@gmail.com>
2018-04-22 23:41:08 +0200

    Player is now in the correct Layer to ignore raycast

--- Assets/_Scripts/Managers/PlayerController.cs
+++ Assets/_Scripts/Managers/PlayerController.cs
@@ -26,6 +26,7 @@ public class PlayerController : MonoBehaviour {
 
     private int direction;
     private bool isInvulnerable = false; //Usar esta variable para ver si el personaje tiene activo el escudo.
+    private bool isEnemyDetected = false;
 
     //Scores
     [SerializeField]
@@ -56,8 +57,11 @@ public class PlayerController : MonoBehaviour {
         anim.SetBool("isAttacking", isAttacking);
 
         Vector3 rayOriginPosition = new Vector3(this.transform.position.x, this.transform.position.y - .5f, this.transform.position.z);
-        Debug.DrawRay(rayOriginPosition, new Vector2(Vector2.right.x * raycastHitRange, 0) , Color.red);
-        Debug.DrawRay(rayOriginPosition, new Vector2(Vector2.left.x, 0) * raycastViewRange, Color.blue);
+        //Debug.DrawRay(rayOriginPosition, new Vector2(Vector2.right.x * raycastHitRange, 0) , Color.red);
+        Debug.DrawRay(rayOriginPosition, new Vector2(-transform.localScale.x, 0) * raycastViewRange, Color.blue);
+
+        DetectEnemyBack();
+
     }
 
     public bool GetIsMoving()
@@ -68,6 +72,22 @@ public class PlayerController : MonoBehaviour {
     public bool GetIsAttacking()
     {
         return isAttacking;
+    }
+
+    private void DetectEnemyBack() {
+        Vector3 rayOriginPosition = new Vector3(this.transform.position.x, this.transform.position.y - .5f, this.transform.position.z);
+        RaycastHit2D hit = Physics2D.Raycast(rayOriginPosition, new Vector2(-transform.localScale.x, 0));
+        // RaycastHit2D hitEnemy = Physics2D.Raycast(rayOriginPosition, Vector2.right);
+
+        if (hit.collider.tag == "Enemy")
+        {
+            //Flip sprite towards enmey
+            Debug.Log("FlipSprite");
+            FlipSprite();
+            isEnemyDetected = true;
+        } else {
+            isEnemyDetected = false;
+        }
     }
 
     /// <summary>
@@ -122,29 +142,8 @@ public class PlayerController : MonoBehaviour {
     {
         //Check closest enemy to player
         //RaycastHit2D hit = Physics2D.Raycast(this.transform.position, new Vector2(this.transform.localScale.x, 0));
-
-        Vector3 rayOriginPosition = new Vector3(this.transform.position.x, this.transform.position.y -.5f, this.transform.position.z);
-        RaycastHit2D hit = Physics2D.Raycast(rayOriginPosition, Vector2.left);
-        RaycastHit2D hitEnemy = Physics2D.Raycast(rayOriginPosition, Vector2.right);
-
-        if (hit.collider.tag == "Enemy") {
-            //Flip sprite towards enmey
-            Debug.Log("FlipSprite");
-
-            FlipSprite();
-        }
-        //Perform Hit with a raycast
-        if (hitEnemy.collider.tag == "Enemy")
-        {
-            Debug.Log("Enemy Hitted");
-        }
-        if (hitEnemy.collider.tag == "Enemy" && Mathf.Abs(Vector3.Distance(hitEnemy.collider.gameObject.transform.position, this.transform.position)) <= raycastHitRange) {
-            //if the raycast hits an enemy destroy the enemy gameobject (maybe send a signal to play death animation?)
-            hitEnemy.collider.gameObject.GetComponent<EnemyBase>().Die(); //Puede que no funciones y haya que mirar el tipo de Enemigo al que accedemos.
-        }
-        //if it doesnt hit anything then keep going.
-
         isAttacking = true;
+
         StartCoroutine(WaitAttack(attackDelay));
 
         return true;
