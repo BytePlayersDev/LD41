3225b38ac9de661f1be712828a59f92cfba161c1
nama1510 <nama1510@gmail.com>
2018-04-23 02:42:12 +0200

    Added Pool for resource management

--- Assets/_Scripts/PlatformSpawner.cs
+++ Assets/_Scripts/PlatformSpawner.cs
@@ -10,9 +10,16 @@ public class PlatformSpawner : MonoBehaviour {
     [SerializeField] private int platformIndex;
     [SerializeField] private GameObject tileGrid;
     [SerializeField] private Transform nextPosition;
+    [SerializeField] private Queue<GameObject> platformPool;
+    [SerializeField] private int maxCapacity = 3;
 
     float timer = 0f;
     #endregion
+    private void Start()
+    {
+        platformPool = new Queue<GameObject>();
+
+    }
     private void FixedUpdate()
     {
         timer += Time.deltaTime;
@@ -29,6 +36,18 @@ public class PlatformSpawner : MonoBehaviour {
         Vector3 pos = new Vector3(transform.position.x, transform.position.y + 8.2f, transform.position.z);
         GameObject go = (GameObject)Instantiate(platforms[platformIndex], pos, Quaternion.identity);
         go.transform.SetParent(tileGrid.transform);
+        platformPool.Enqueue(go);
+
+        Debug.Log(platformPool.Count);
+
+        if (platformPool.Count >= maxCapacity) {
+            Debug.Log(platformPool.Peek().name + " Eliminated new count is " + platformPool.Count);
+            GameObject aux = platformPool.Dequeue();
+            Destroy(aux);
+        }
+
+        //platformPool.Push(go);
+        
     }
     void RandomIndex() {
         platformIndex = Random.Range(0, platforms.Length);
