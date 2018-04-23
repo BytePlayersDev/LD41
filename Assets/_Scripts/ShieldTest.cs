9780437b66f0dbb1c0915f9b93a4c7493751e652
Varo Hurtado <ahs007@gmail.com>
2018-04-23 14:18:34 +0200

    Shield_Timer uploaded as Prefab.

---
+++ Assets/_Scripts/ShieldTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldTest : MonoBehaviour {
+
+    public Animator anim;
+    public float seconds;
+        
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(ShieldTimer(seconds));
+	}
+
+    IEnumerator ShieldTimer(float seconds)
+    {
+        float fixedShield = seconds / 12;
+
+        for (int i = 13; i > 0; --i)
+        {
+            anim.SetFloat("porcentaje", i);
+            yield return new WaitForSeconds(fixedShield);
+        }
+    }
+}
