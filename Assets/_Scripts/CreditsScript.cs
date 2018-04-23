b4f96fdd969c2131fd5ea606c3a414782487440a
Varo Hurtado <ahs007@gmail.com>
2018-04-23 04:05:36 +0200

    Credits completed. New assets updated.

---
+++ Assets/_Scripts/CreditsScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour {
+        
+    void Start()
+    {
+        StartCoroutine(WaitCredits());
+    }
+
+    IEnumerator WaitCredits()
+    {
+        yield return new WaitForSeconds(40);
+        SceneManager.LoadScene("Menu");
+    }
+}
