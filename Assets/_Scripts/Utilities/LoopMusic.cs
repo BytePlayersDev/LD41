ffe291c8e46e46667b2aba9667f59c42c8f87384
Varo Hurtado <ahs007@gmail.com>
2018-04-24 02:59:18 +0200

    JAM FINISHED

---
+++ Assets/_Scripts/Utilities/LoopMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopMusic : MonoBehaviour {
+
+    public AudioSource musicBase;
+    public AudioClip musicClip;
+
+    void Update ()
+    {
+  //      if(!musicBase.isPlaying)
+  //      {
+  //          musicBase.clip = musicClip;
+  //          musicBase.Play();
+  //          musicBase.loop = true;
+  //      }
+	}
+}
